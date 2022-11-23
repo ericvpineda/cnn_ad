2bbf6e37cd53ff52332ee3ad49bdf20780bf174d
Eric Pineda <ericvpineda@berkeley.edu>
2022-11-22 23:37:23 -0800

    implemented -- basic 6 layer cnn

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Resources 
+- kaggle dataset: https://www.kaggle.com/datasets/tourist55/alzheimers-dataset-4-class-of-images?resource=download
---
+++ utils.py
@@ -0,0 +1,32 @@
+import os 
+from PIL import Image 
+import numpy as np 
+
+def get_image_properties(directory, resize_dim):
+    
+    pixels, labels = [], [] 
+    all_folders = os.listdir(directory)
+    for i in range(len(all_folders)):
+
+        foldername = all_folders[i]
+        folder_path = directory + "/" + foldername
+        folder_files = [name for name in os.listdir(folder_path)]
+        
+        if len(folder_files) > 0:
+
+            for filename in os.listdir(folder_path):
+
+                file_path = folder_path + '/' + filename
+                # Convert image to greyscale 
+                image = Image.open(file_path).convert('1')
+                if resize_dim != None:
+                    image = image.resize(resize_dim, Image.ANTIALIAS)
+                image_pixels = list(image.getdata())
+                pixels.append(image_pixels)
+                labels.append(i)
+
+    pixels = np.array(pixels)
+    labels = np.array(labels)
+    # Normalize pixel intensities
+    pixels = pixels / 255.0
+    return [pixels, labels]
